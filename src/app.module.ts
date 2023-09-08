552c78e2b027681c8473098b706245f09cb2c8fe
naufal <naufal.razzan@hotmail.com>
2023-09-08 13:28:12 +0700

    minor update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './models/user.entity';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.POSTGRES_URI
-    })
+      url: "postgres://stqakvgy:Is94dqAOr9bGbxhoLpRD_BXN5FgfxPb0@rain.db.elephantsql.com/stqakvgy",
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+    AuthModule,
+    UsersModule
   ],
   controllers: [],
   providers: [],
