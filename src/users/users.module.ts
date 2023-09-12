e3607123f1537aafbe854358c26959c47e68e8dd
naufal <naufal.razzan@hotmail.com>
2023-09-12 07:49:46 +0700

    modify users folder

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from 'src/models/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
+  exports: [UsersService],
+  controllers: [],
   providers: [UsersService]
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,26 +11,11 @@ export class UsersService {
         private readonly userRepository: Repository<User> 
     ){}
 
-    async insertOne(username: string, email: string, role: string, password: string): Promise<User> {
-        try {
-            const hashedPassword = await bcrypt.hash(password, 10)
-        
-            const user = new User()
-            user.name = username
-            user.email = email
-            user.role = role
-            user.password = hashedPassword
-
-            return await this.userRepository.save(user)
-        } catch (err) {
-            if(err instanceof QueryFailedError && err.message.includes('duplicate key value violates unique constraint')){
-                throw new HttpException('Username or email is already in use', HttpStatus.CONFLICT)
-            }
-            throw err
-        }
+    async insertOne(user: User): Promise<User> {
+        return await this.userRepository.save(user)
     }
 
-    async findOne(email: string): Promise<User>{
+    async findUser(email: string): Promise<User>{
         return await this.userRepository.findOne({
             where: {
                 email: email
