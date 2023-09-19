4aa0b75b3f2c216fc54daa6b7be94a23fff8bf02
naufal <naufal.razzan@hotmail.com>
2023-09-19 19:26:11 +0700

    add class validator in prop

--- src/models/dto/create-movie.dto.ts
+++ src/models/dto/create-movie.dto.ts
@@ -1,8 +1,22 @@
+import { IsLowercase, IsNotEmpty, IsString, NotContains } from "class-validator";
 
 
 export class CreateMovieDto {
+  @IsString({message: 'title must be a type of string'})
+  @IsNotEmpty({message: 'title cannot be empty'})
   title: string;
+
+  @IsString({message: 'code title cannot be empty'})
+  @IsLowercase({message: 'code title must be lowercase'})
+  @NotContains(' ', {message: 'code title cannot have a whitespace'})
+  codeTitle: string
+
+  @IsString({message: 'genres must be a type of string'})
   genres: string;
+
+  @IsString({message: 'duration must be a type of string'})
   duration: string;
+
+  @IsString({message: 'rating must be a type of string'})
   rating: string;
 }
